7d7d309b5f352ab330dbd84c59b3796188d49e79
samerlagha <samer_la@mail.ru>
2020-03-24 13:21:55 +0200

    commit

---
+++ lesson1/todo-list/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+
+  <div className="title">Todo List</div>
+);
+
+ReactDOM.render(element,rootElement);
