965e032c07e3476da08d683b43b055a622fa04f1
samerlagha <samer_la@mail.ru>
2020-03-25 16:54:20 +0200

    commit

--- lesson3/task3/src/Comment.jsx
+++ lesson3/task3/src/Comment.jsx
@@ -6,14 +6,21 @@ import './comment.scss';
 
 const formatDate = date => moment(date).format("DD MMM YYYY");
 
-const Comment = props => {
-    return (
-        <div className="comment">
-            <UserInfo author={props.author}/>
-            <div className="comment__text">{props.text}</div>
-            <div className="comment__date">{formatDate(props.date)}</div>
-        </div>
-    );
+function Comment(props) {
+  return (
+    <div className="comment">
+      <div className="user-info">
+        <img
+          className="avatar"
+          src={props.user.avatarUrl}
+          alt={props.user.name}
+        />
+        <div className="user-info__name">{props.user.name}</div>
+      </div>
+      <div className="comment__text">{props.text}</div>
+      <div className="comment__date">{formatDate(props.date)}</div>
+    </div>
+  );
 }
 
 export default Comment;
