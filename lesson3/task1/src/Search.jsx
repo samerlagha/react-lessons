9bd902d245eef8e81b99c6f337a264792df71d5c
samerlagha <samer_la@mail.ru>
2020-03-25 13:59:19 +0200

    commit

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+// import React from 'react';
 
-const Search = (props) => {
-    return (
-        <div className="search">
-            <h1 className="serch__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-            <div className="search__field">
-                <input type="text" className="search__input"/>
-                <button className="search__button">Search</button>
-            </div>
-        </div>
-    );
-};
+// const Search = (props) => {
+//     return (
+//         <div className="search">
+//             <h1 className="serch__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+//             <div className="search__field">
+//                 <input type="text" className="search__input"/>
+//                 <button className="search__button">Search</button>
+//             </div>
+//         </div>
+//     );
+// };
 
 
-export default Search; 
+// export default Search; 
--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 import './search.scss';
 
-import Search from './Search.jsx';
+// import Search from './Search.jsx';
 
 const rootElement = document.querySelector('#root');
+const Search = (props) => {
+    return (
+        <div className="search">
+            <h1 className="serch__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+            <div className="search__field">
+                <input type="text" className="search__input"/>
+                <button className="search__button">Search</button>
+            </div>
+        </div>
+    );
+};
+
 
 ReactDOM.render(<Search name="Tom" />, rootElement);
