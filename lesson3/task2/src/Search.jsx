00bcd01a4d656b8e49555653422481b093561f4d
samerlagha <samer_la@mail.ru>
2020-03-26 13:23:51 +0200

    correct

--- lesson3/task2/src/Search.jsx
+++ lesson3/task2/src/Search.jsx
@@ -6,7 +6,7 @@ class Search extends Component {
 
         return (
             <div className="search">
-                <h1 className="serch__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+                <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
                 <div className="search__field">
                     <input type="text" className="search__input" />
                     <button className="search__button">Search</button>
