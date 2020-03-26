463c7ca428bce772158bf07d9f311128cb9fd384
samerlagha <samer_la@mail.ru>
2020-03-26 17:17:37 +0200

    commit

--- lesson4/task1/src/App.jsx
+++ lesson4/task1/src/App.jsx
@@ -6,9 +6,9 @@ import Counter from './Counter';
 const App = () => {
     return (
       <>
-        <Counter start={5} interval={1000} />
-        <Counter start={12} interval={2000} />
-        <Counter start={-21} interval={100} />
+        <Counter start={8} interval={1000} />
+        <Counter start={17} interval={2000} />
+        <Counter start={-50} interval={100} />
       </>
     );
   };
