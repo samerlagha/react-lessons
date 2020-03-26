b3dafe9df6c0d85479775e125987cd9d05f99659
samerlagha <samer_la@mail.ru>
2020-03-26 16:59:04 +0200

    commit

---
+++ lesson4/task1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Counter from './Counter';
+
+
+const App = () => {
+    return (
+      <>
+        <Counter start={5} interval={1000} />
+        <Counter start={12} interval={2000} />
+        <Counter start={-21} interval={100} />
+      </>
+    );
+  };
+  export default App;
