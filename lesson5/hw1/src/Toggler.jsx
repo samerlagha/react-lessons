387ca07d8c7efb9ae8ecf2882921e0aab123cb9a
samerlagha <samer_la@mail.ru>
2020-03-30 20:57:50 +0300

    commit

---
+++ lesson5/hw1/src/Toggler.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Toggler extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      text: 'Off'
+    };
+  }
+  changeText(){
+    this.setState({
+      text: this.state.text === 'Off' 
+        ? 'On'
+        : 'Off'
+    })
+  }
+  render(){
+    return (
+      <button 
+        className="toggler"
+        onClick={() => this.changeText()}
+      >
+          {this.state.text}
+      </button>
+    );
+  }
+}
+export default Toggler;
