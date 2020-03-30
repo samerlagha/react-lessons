aa012a9583c2addaea5d8d75f4b8f859ff6476d6
samerlagha <samer_la@mail.ru>
2020-03-30 13:22:17 +0300

    event-class-react

---
+++ lesson5/task2/src/GoodButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class GoodButton extends React.Component{
+
+  handleClick(){
+
+  }
+    render(){
+
+        return(
+            <button 
+            className="fancy-button"
+            onClick={this.handleClick} >
+            Click me
+          </button>
+        )
+    }
+}
+
+export default GoodButton;
