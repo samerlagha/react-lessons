594c22492b096dd55773771df1e5d0ad951c7eda
samerlagha <samer_la@mail.ru>
2020-03-31 20:15:46 +0300

    commit

---
+++ lesson6/task3/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+import Page from './Page';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Page />, rootElem);
