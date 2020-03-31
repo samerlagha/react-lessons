bb0db95f6fecc7876ef045291f93e0a4a2366bc2
samerlagha <samer_la@mail.ru>
2020-03-31 15:59:35 +0300

    commit

--- lesson6/task1/src/Auth.jsx
+++ lesson6/task1/src/Auth.jsx
@@ -1,14 +1,34 @@
-import React  from 'react';
-import Greeting from './Greeting'
+import React, { Component } from 'react';
+import Greeting from './Greeting';
+import Login from './Login';
+import Logout from './Logout';
 
-class Auth extends React.Component{
-
-    render(){
-        return(
-            <div className="panel">
-                <Greeting />
-            </div>
-        );
-    }
+class Auth extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  handleLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    });
+  };
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false
+    });
+  };
+  render(){
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+        {this.state.isLoggedIn
+          ? <Logout isLogin={this.handleLogout} />
+          : <Login isLogin={this.handleLogin} />}
+      </div>
+    );
+  }
 }
 export default Auth;
