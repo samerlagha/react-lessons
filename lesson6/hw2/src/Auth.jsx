2ca0612627cabb5a9fa2bcc39c9922f71ea76068
samerlagha <samer_la@mail.ru>
2020-04-01 16:13:17 +0300

    commit

--- lesson6/hw2/src/Auth.jsx
+++ lesson6/hw2/src/Auth.jsx
@@ -1,86 +1,41 @@
-import React, { Component } from "react";
-import Login from "./Login";
-import Logout from "./Logout";
-import Spinner from "./Spinner";
+import React, { Component } from 'react';
+import Login from './Login';
+import Logout from './Logout';
+import Spinner from './Spinner';
 
 class Auth extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogin: true,
-      spinner: false
-    };
-  }
-
+  state = {
+    isLogin: true,
+    Spinner: false
+  };
   onLogin = () => {
-    this.setState({
-      spinner: true,
-      isLogin: false
-    });
+    this.changeSpinner();
+    this.setState({ isLogin: true });
+  };
+  onLogout = () => {
+    this.changeSpinner();
+    this.setState({ isLogin: false });
+  };
+  changeSpinner = () => {
     setTimeout(() => {
       this.setState({
-        spinner: false
+        Spinner: false
       });
     }, 2000);
-  };
-
-  onLogout = () => {
     this.setState({
-      isLogin: true,
-      spinner: false
+      Spinner: true
     });
   };
-
   render() {
-    return (
-      <>
-        {this.state.isLogin ? (
-          <Login onLogin={this.onLogin} />
-        ) : (
-          <Logout onLogout={this.onLogout} />
-        )}
-        {this.state.spinner && <Spinner size={25} />}
-      </>
-    );
-  }
-}
+    if (this.state.Spinner) {
+      return <Spinner size={40} />;
+    }
+    return this.state.isLogin
+      ? <Login onLogin={this.onLogout} />
+      : <Logout onLogout={this.onLogin} />
+  };
+};
+
 export default Auth;
-// import React, { Component } from 'react';
-// import Login from './Login';
-// import Logout from './Logout';
-// import Spinner from './Spinner';
 
-// class Auth extends Component {
-//   state = {
-//     onlineSatus: true,
-//     markForSpinner: false
-//   };
-//   handleLogin = () => {
-//     this.changeMarkForSpinner();
-//     this.setState({ onlineSatus: true });
-//   };
-//   handleLogout = () => {
-//     this.changeMarkForSpinner();
-//     this.setState({ onlineSatus: false });
-//   };
-//   changeMarkForSpinner = () => {
-//     setTimeout(() => {
-//       this.setState({
-//         markForSpinner: false
-//       });
-//     }, 2000);
-//     this.setState({
-//       markForSpinner: true
-//     });
-//   };
-//   render() {
-//     if (this.state.markForSpinner) {
-//       return <Spinner size={40} />;
-//     }
-//     return this.state.onlineSatus
-//       ? <Login onLogin={this.handleLogout} />
-//       : <Logout onLogout={this.handleLogin} />
-//   };
-// };
 
-// export default Auth;
