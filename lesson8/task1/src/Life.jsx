71ff63ee86a6ead92cd71fcf9cc2dc5c43ad8460
samerlagha <samer_la@mail.ru>
2020-04-03 09:15:24 +0300

    commit

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -3,7 +3,38 @@ import React, { Component } from 'react';
 class Life extends Component {
     constructor(props) {
         super(props);
-        console.log('constructor')
+        console.log(import React, { Component } from 'react';
+
+        class Life extends Component {
+            constructor(props) {
+                super(props);
+                console.log('constructor')
+            }
+        
+            componentDidMount() {
+                console.log('componentDidMount: API calls, subscriptions')
+            }
+        
+            shouldComponentUpdate(nextProps, nextState) {
+                console.log('shoudlComponentUpdate(nextProps, nextState): decide to render or not to render');
+                return true;
+            }
+        
+            componentDidUpdate(prevProps, prevState) {
+                console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
+            }
+        
+            componentWillUnmount() {
+                console.log('componentWillUnmount(): cleanup before DOM related to component will be removed')
+            }
+        
+            render() {
+                console.log('return React element to build DOM' )
+                return <div className="number">{this.props.number}</div>;
+            }
+        }
+        
+        export default Life;)
     }
 
     componentDidMount() {
