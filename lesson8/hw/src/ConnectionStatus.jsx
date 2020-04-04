c44e2626516420a49ed163d58a3e81175e414e83
samerlagha <samer_la@mail.ru>
2020-04-04 09:47:02 +0300

    commit

---
+++ lesson8/hw/src/ConnectionStatus.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class ConnectionStatus extends React.Component {
+
+ constructor(props){
+     super(props);
+     this.state={
+         status:true,
+     }
+ }
+ componentDidMount(){
+    window.addEventListener('offline', this.offlineToggler);
+    window.addEventListener('online', this.onlineToggler);
+ }
+
+ UNSAFE_componentWillMount (){
+    window.addEventListener('offline', this.offlineToggler);
+    window.addEventListener('online', this.onlineToggler);
+ }
+
+  offlineToggler =()=>{
+      this.setState({
+        status:true
+        
+      })
+  }
+  
+  onlineToggler =()=>{
+      this.setState({
+          status:false
+      })
+  }
+    render() {
+        return(
+            this.state.status ?
+            <div className="status">online</div> :
+            <div className="status status_offline">Offline</div>
+        );
+    }
+
+    }
+  
+
+export default ConnectionStatus;
