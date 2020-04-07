9769dd471d1a9b2195ecc1b8d73bb4e807475de0
samerlagha <samer_la@mail.ru>
2020-04-07 08:51:25 +0300

    commit

---
+++ lesson9/task2/src/UserForm.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+
+export default class UserForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            student: '',
+            occupation: '',
+            about: '',
+        }
+    }
+    
+    onSubmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state);
+    }
+
+    handleChange = event => {
+        const { name, value, checked, type } = event.target;
+
+        const val = type === 'checkbox'
+            ? checked
+            : value;
+        this.setState({
+            [name]: val,
+        });
+    }
+
+    render() {
+        return (
+            <form className="login-form" onSubmit={this.onSubmit}>
+                <h1 className="form-title">Profile</h1>
+                <div className="form-control">
+                    <label className="form-label" htmlFor="name">Name</label>
+                    <input
+                        className="form-input"
+                        value={this.state.name}
+                        type="text"
+                        id="name" name="name"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="form-control">
+                    <label className="form-label" htmlFor="student">Student</label>
+                    <input 
+                        className="form-input" 
+                        value={this.state.student} 
+                        type="checkbox" id="student" 
+                        name="student"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="form-control">
+                    <label className="form-label" id="occupation" htmlFor="occupation">Occupation</label>
+                    <select 
+                        name="occupation" 
+                        value={this.state.occupation} 
+                        className="form-input"
+                        onChange={this.handleChange}
+                    >
+                        <option value="london">London</option>
+                        <option value="new-york">New York</option>
+                        <option value="sidney">Sidney</option>
+                        <option value="berlin">Berlin</option>
+                    </select>
+                </div>
+                <div className="form-control">
+                    <label className="form-label" id="about" htmlFor="about">About</label>
+                    <textarea
+                        name="about" 
+                        value={this.state.about} 
+                        className="form-input"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <button className="submit-button" type="submit">Submit</button>
+            </form>
+        )
+    }
+}
