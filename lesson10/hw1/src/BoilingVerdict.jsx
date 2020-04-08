884bcf4e1c7ceb6a713e046cacd247da4d9e7447
samerlagha <samer_la@mail.ru>
2020-04-08 11:34:59 +0300

    commit

--- lesson10/hw1/src/BoilingVerdict.jsx
+++ lesson10/hw1/src/BoilingVerdict.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const BoilingVerdict =(props)=>{
-
-    if(props.celsius >=100){
-        return <p>The water would boil.</p>
+const BoilingVerdict = props => {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
     }
-    return <p>the water would not boil.</p>
-}
+    return <p>The water would not boil.</p>
+};
+
 export default BoilingVerdict;
