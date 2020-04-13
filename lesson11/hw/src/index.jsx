ecd67c185bdf1b4af22a29f07a569d3076760eeb
samerlagha <samer_la@mail.ru>
2020-04-13 14:25:40 +0300

    commit

--- lesson11/hw/src/index.jsx
+++ lesson11/hw/src/index.jsx
@@ -9,7 +9,7 @@ const rootElem = document.querySelector('#root');
 const title="Some title";
 ReactDOM.render(<Expand title={title} > 
 
-<p>
+   <p>
     Hooks are a new addition in React 16.8. They let you use state and other \React features without writing a className.
     </p>
 </Expand>, rootElem);
