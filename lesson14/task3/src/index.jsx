357fc6439534995248b5c96d070b256c85d1edc9
samerlagha <samer_la@mail.ru>
2020-05-06 15:35:45 +0300

    commit

---
+++ lesson14/task3/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dimensions from './Dimensions';
+import './index.scss';
+
+ReactDOM.render(<Dimensions />, document.querySelector('#root'));
